0ffcb4249d5b3e0bd3f046d604abfc3e82be7bd7
MarcosRojas <mrojas40187@ufide.ac.cr>
2023-10-19 22:41:19 -0600

    Se agrega la clase cancion

---
+++ src/colegiado1marcosrojas/Cancion.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package colegiado1marcosrojas;
+
+/**
+ *
+ * @author Marco
+ */
+public class Cancion {
+    public int id;
+    public int anio;
+    public String titulo;
+    public String autor;
+    public String duracion;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAnio() {
+        return anio;
+    }
+
+    public void setAnio(int anio) {
+        this.anio = anio;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getDuracion() {
+        return duracion;
+    }
+
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
+    }
+    
+    
+    
+}
--- src/colegiado1marcosrojas/Colegiado1MarcosRojas.java
+++ src/colegiado1marcosrojas/Colegiado1MarcosRojas.java
@@ -1,3 +1,4 @@
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -15,6 +16,21 @@ public class Colegiado1MarcosRojas {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Cancion cancion1 = new Cancion();
+        
+        cancion1.setId(6756);
+        cancion1.setAnio(1984);
+        cancion1.setAutor("Michael Jackson");
+        cancion1.setDuracion("5:58");
+        cancion1.setTitulo("Thriller");
+        
+        System.out.println(cancion1.getId());
+        System.out.println(cancion1.getAnio());
+        System.out.println(cancion1.getAutor());
+        System.out.println(cancion1.getDuracion());
+        System.out.println(cancion1.getTitulo());
+
+        
     }
     
 }
